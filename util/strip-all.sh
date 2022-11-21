b38850321df83ca4f08db703781d360ad3d07f4c
Özgür Akgün <ozgurakgun@gmail.com>
2022-11-21 09:28:15 +0000

    some utility scripts

---
+++ util/strip-all.sh
@@ -0,0 +1,3 @@
+du -sh .
+strip z3 yices-smt2 yices-smt yices-sat yices open-wbo nbc_minisat_all_release treengeling plingeling lingeling fzn-gecode fzn-chuffed kissat cadical boolector bc_minisat_all_release glucose glucose-syrup conjure
+du -sh .
